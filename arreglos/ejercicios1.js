9453ef32490822f3a644240c83e21924b86608c8
Emilixipixi <carlosemilio270798@gmail.com>
2025-10-16 23:20:14 -0500

    Promedio Notas

---
+++ arreglos/ejercicios1.js
@@ -0,0 +1,44 @@
+let notas=[];
+
+agregarElementos=function(){
+    notas.push(5);
+    notas.push(10);
+    console.log(notas.length);
+}
+
+recorrerArreglo=function(){
+    let notasR;
+    for(let indice=0;indice<notas.length;indice++){
+       notasR= notas[indice];
+       console.log(notasR);
+    }
+}
+
+probarAgregar=function(){
+    let notaRecuperada;
+    notaRecuperada=recuperarInt("txtNota");
+    agregarNota(notaRecuperada);
+}
+
+agregarNota=function(nota){
+    notas.push(nota);
+}
+
+calcularPromedio = function() {
+    let sumaNotas = 0;
+    let promedio;
+
+    for (let i = 0; i < notas.length; i++) {
+        sumaNotas += notas[i];
+    }
+
+    promedio = sumaNotas / notas.length;
+    return promedio;
+}
+
+
+ejecutarPromedio=function(){
+    let promedioFinal=calcularPromedio();
+    document.getElementById("lblResultado").innerText =promedioFinal;
+
+}
